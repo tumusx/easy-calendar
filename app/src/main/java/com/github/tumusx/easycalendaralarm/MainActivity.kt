40d69395f6bd2bb6e2916cffc42f4ccce66e868f
Murillo Alves <tumusx1@gmail.com>
2024-03-23 16:39:06 -0300

    first commit

---
+++ app/src/main/java/com/github/tumusx/easycalendaralarm/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.github.tumusx.easycalendaralarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.tumusx.easycalendaralarm.ui.theme.EasyCalendarAlarmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EasyCalendarAlarmTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EasyCalendarAlarmTheme {
+        Greeting("Android")
+    }
+}
